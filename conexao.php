15c8597db98a77764cbabcd39c354b5486ef040c
Paulo-r-joao <paulo_r_joao@estudante.sesisenai.org.br>
2025-09-02 15:05:45 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -3,9 +3,10 @@ $host = "localhost";
 $dbname = "carrinhocheio";
 $usuario = "root";
 $senha = "";
+$port = "3306";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $usuario, $senha);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $usuario, $senha);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro na conexão: " . $e->getMessage());
